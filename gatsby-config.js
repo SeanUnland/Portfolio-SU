ec01afa6f94539a2fdc7e06833efd58c33a25dfc
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-12-03 16:06:23 -0500

    portfolio

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
-        path: `${__dirname}/src/data/`,
+        path: `/src/data/`,
       },
     },
   ],
