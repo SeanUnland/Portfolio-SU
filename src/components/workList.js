7bd79fedcdb6f413ae3766741c77566eedf908ab
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-12-09 18:01:03 -0500

    portfolio

--- src/components/workList.js
+++ src/components/workList.js
@@ -25,10 +25,18 @@ const WorkList = props => {
           <Card.Body>
             <Card.Title>Mini-Pop</Card.Title>
             <Card.Text>
-              A synth step sequencer Built using Tone.js and React
+              A synth step sequencer and sampler.
+              <br />
+              Built using Tone.js and React
             </Card.Text>
             <Button href="https://mini-pop.netlify.app/" variant="outline-dark">
               Mini-Pop
+            </Button>
+            <Button
+              href="https://github.com/SeanUnland/unit-4-project-mini-pop"
+              variant="outline-dark"
+            >
+              Code
             </Button>
           </Card.Body>
         </Card>
@@ -49,6 +57,12 @@ const WorkList = props => {
             >
               Sticky Task
             </Button>
+            <Button
+              href="https://github.com/E-Shelton98/StickyTask-Frontend"
+              variant="outline-dark"
+            >
+              Code
+            </Button>
           </Card.Body>
         </Card>
       </div>
@@ -58,10 +72,18 @@ const WorkList = props => {
           <Card.Body>
             <Card.Title>TUNR</Card.Title>
             <Card.Text>
-              A music playlist app built using React and Ruby on Rails
+              A music playlist app
+              <br />
+              Built using React and Ruby on Rails
             </Card.Text>
             <Button href="https://tunr-su.netlify.app/" variant="outline-dark">
               TUNR
+            </Button>
+            <Button
+              href="https://github.com/SeanUnland/rails-mini-project-frontend"
+              variant="outline-dark"
+            >
+              Code
             </Button>
           </Card.Body>
         </Card>
