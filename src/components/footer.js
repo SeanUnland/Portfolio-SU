5f7b04d8f4b8895f895f1d38539f764593b0a872
Sean Unland <seanunland@Seans-MacBook-Pro.local>
2020-12-17 11:11:36 -0800

    portfolio

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,6 +5,18 @@ const Footer = props => {
   return (
     <div id="footer">
       <p>© 2020 Sean Unland</p>
+      <div id="linkedIn-icon">
+      <a href="https://www.linkedin.com/in/sean-unland-b46b2743/">
+        <img id="linkedIn" src="https://img.icons8.com/ios/50/000000/linkedin.png"/>
+      </a>
+      </div>
+      <div id="gitHub-icon">
+      <a href="https://github.com/SeanUnland">
+        <img id="gitHub" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/240px-Octicons-mark-github.svg.png"/>
+      </a>
+      </div>
+      
+      
     </div>
   )
 }
