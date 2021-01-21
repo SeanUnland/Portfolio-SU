3a42f19c1a1680815d2a587d65f99bcf87dd3b0f
Sean Unland <seanunland@Seans-MacBook-Pro.local>
2021-01-21 11:09:51 -0500

    portfolio

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import "./footer.css"
 const Footer = props => {
   return (
     <div id="footer">
-      <p>© 2020 Sean Unland</p>
+      <p>© 2021 Sean Unland</p>
       <div id="linkedIn-icon">
       <a href="https://www.linkedin.com/in/sean-unland-b46b2743/">
         <img id="linkedIn" src="https://img.icons8.com/ios/50/000000/linkedin.png"/>
