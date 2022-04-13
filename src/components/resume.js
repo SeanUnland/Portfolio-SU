91d0698546809f610d2c5d19ce749d7e6ea73f6f
Sean Unland <sdunland@gmail.com>
2022-04-13 13:59:13 -0400

    Updated Resume on About Page

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react"
 import "./resume.css"
 
 const Resume = props => {
-    return (
-        <div>
-            <iframe src="https://drive.google.com/file/d/1p2cYCwWw47VoKLWpCMz7xqiJibm3uFl3/preview" width="600" height="500"></iframe>
-            
-        </div>
-    )
+  return (
+    <div>
+      <iframe
+        src="https://drive.google.com/file/d/1FDvcIEKjYEVK1hS9X3XID_NW22ucVSZO/preview"
+        width="640"
+        height="480"
+        allow="autoplay"
+      ></iframe>
+    </div>
+  )
 }
-
-
 
 export default Resume
