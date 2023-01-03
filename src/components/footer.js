c7fbd2f94c7a88874f492379a3c76311fea0c8b6
Sean Unland <sdunland@gmail.com>
2023-01-03 13:11:07 -0500

    updated copyright year

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import "./footer.css"
 const Footer = props => {
   return (
     <div id="footer">
-      <p>© 2022 Sean Unland</p>
+      <p>© 2023 Sean Unland</p>
       <div id="linkedIn-icon">
         <a href="https://www.linkedin.com/in/sean-unland-b46b2743/">
           <img
